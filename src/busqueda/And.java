de21b544ce1258ce78aa57d5aa7fe06a8197f203
Jazzy <jazminberengo@gmail.com>
2026-06-26 20:49:27 -0300

    Implementar especificaciones de busqueda compuesta

--- src/busqueda/And.java
+++ src/busqueda/And.java
@@ -1,5 +1,24 @@
 package busqueda;
 
-public class And {
+import catalogo.ItemCatalogo;
+import java.util.List;
+
+public class And implements CriterioBusqueda{
+
+	private List<CriterioBusqueda> criterios;
+	
+	public And(List<CriterioBusqueda> criterios) {
+		this.criterios = criterios;
+	}
+	
+	@Override
+	public boolean cumple(ItemCatalogo item) {
+		for (CriterioBusqueda criterio : this.criterios) {
+			if (!criterio.cumple(item)) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 }
